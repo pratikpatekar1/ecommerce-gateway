f4a553d07038bc3dc70a3742e7ec555d67d03cb2
pratikpatekar1 <2016bcs042@sggs.ac.in>
2024-03-29 03:32:08 +0530

    implemented spring cloud gateway and load balancer

---
+++ src/main/java/dev/zoro/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.zoro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
